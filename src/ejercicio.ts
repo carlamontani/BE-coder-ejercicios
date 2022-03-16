5cd80ab7c75cacf7fe5c35ca06cf1a7c010a36a5
carlamontani <carlamontani@gmail.com>
2022-03-15 22:56:51 -0300

    fin integracion con las clases 💪

---
+++ src/ejercicio.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from "express";
+let Contenedor = require('./class/Contenedor');
+const contenedor = new Contenedor('productos.txt')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("./src/public"))
+
+const routerProductos = express.Router();
+
+
+routerProductos.get("/", (req: Request, res: Response) => {
+  //res.status(200).json(productos);
+  async function run() {
+    res.status(200).json(await contenedor.getAll())
+  }
+  run()
+});
+
+routerProductos.get("/:id", (req: Request, res: Response) => {
+  const idRequested = Number(req.params.id)
+
+  async function run() {
+    const result = await contenedor.getById(idRequested)
+    if (result.length === 1) { 
+      res.status(200).json({
+        productoBuscado: result,
+      })
+    } else {
+      res.status(404).json({
+        error: 'producto no encontrado',
+      })
+    }
+  }
+  run()
+});
+
+//POST
+routerProductos.post("/", (req: Request, res: Response) => {
+  const { body } = req;
+  /*
+  const lastProduct = productos.slice(-1)[0]
+
+  productos.push({...body, id : lastProduct.id+1})
+
+  res.status(200).json({
+    productos,
+  });*/
+
+  async function run() {
+    res.status(200).json(await contenedor.save(body)) //devuelve el nuevo id generado, cambiar si quiero devolver la lista nueva
+  }
+  run()
+});
+
+//PUT
+routerProductos.put("/:id", (req: Request, res: Response) => {
+  const { body } = req;
+  const { params } = req;
+
+  async function run() {
+    res.status(200).json(await contenedor.update(body, params)) //devuelve el nuevo id generado, cambiar si quiero devolver la lista nueva
+  }
+  run()
+});
+
+//delete
+routerProductos.delete("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  /*
+  for (var i = 0; i < productos.length; i++) {
+    var obj = productos[i];
+
+    if (Number(id) === obj.id) {
+      productos.splice(i, 1);
+    }
+  }
+
+  res.status(200).json({
+    productos,
+  });*/
+
+  async function run() {
+    res.status(200).json(await contenedor.deleteById(id)) //ver que onda los ids
+  }
+  run()
+});
+
+app.use("/productos", routerProductos)
+
+const PORT = 8080
+const server = app.listen(PORT, () =>
+console.log(`Server en port http://localhost:8080`)
+)
+
+server.on('error', (error:any) => console.log(error))
