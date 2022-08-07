b53fc239ef4b9ff4a65de4304e04d74e88e4bdd9
carlamontani <carlamontani@gmail.com>
2022-08-06 21:25:34 -0300

    pruebas deno

---
+++ types/server.ts
@@ -0,0 +1,11 @@
+import { Application } from "./deps.ts";
+import UserRoute from "./routes/user.route.ts";
+import { logger } from "./middlewares/logger.middleware.ts";
+
+const app = new Application();
+
+app.use(logger);
+app.use(UserRoute.routes());
+
+console.log("Server running on http://localhost:3000");
+await app.listen({ port: 3000 });
